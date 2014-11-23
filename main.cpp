4a4eec3aa0a94bc0604dbaff1e7c2d0d366b60d7
sarcastiksh0rtee37 <Rar09a@acu.edu>
2014-11-23 17:52:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,20 @@ int main()
     // make all places, people, stuff, and things in game
     Game one;
     
+
     // input string
-    string input;
+    string input="start";
     
+    static bool finished=false;
+    while(!finished && input!="quit")
+    {
+        alice.whereareyou()->narrate(cout);
+        
+        cout << "What do you want to do?" << endl << "Keywords: aboutme, go, pickup, drop, use, approach" << endl;   //we need to write approach for alice so she can approach a person, or a thing
+        cin >> input;
+        one.delegate(input)
+    }
+
     // start with description of Alice
     //one.getPeopleList().peek(6)->narrate(cout);
     
