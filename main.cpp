4217c8e6a84a54652186027326fab07439bff934
sarcastiksh0rtee37 <Rar09a@acu.edu>
2014-11-25 12:13:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main()
 
     // input string
     string input="start";
-    
     one.findHere()->whoHere().find("Alice")->second->render(cout);
-    one.findHere()->narrate(cout);
+
     
     static bool finished=false;
     while(!finished && input!="quit")
     {
+            one.findHere()->narrate(cout);
         cout << "What do you want to do?" << endl << "Keywords: aboutme, go, pickup, drop, use, approach" << endl;
         
         //we need to write approach for alice so she can approach a person, or a thing
